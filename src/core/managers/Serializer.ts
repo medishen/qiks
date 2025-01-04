9bf9de7d968ca0a833bbd5f74e10bebf8333f1aa
mahdi <bitsgenix@gmail.com>
2025-01-04 14:43:48 +0330

    fix(serializer): enhance serialization/deserialization for complex types
    - Added support for serializing/deserializing `Object` with nested properties.
    - Improved `Date`, `RegExp`, `Function`, `Map`, and `Buffer` handlers for better accuracy and structure.
    - Fixed `ArrayBuffer` deserialization to properly reconstruct buffers.
    - Prevented duplicate serialization using `__SERIALIZED__` marker.

--- src/core/managers/Serializer.ts
+++ src/core/managers/Serializer.ts
@@ -14,33 +14,86 @@ type Handlers = {
   Error: Handler<Error>;
   Promise: Handler<Promise<any>>;
   ArrayBuffer: Handler<ArrayBuffer>;
+  Object: Handler<Object>;
 };
 
 export class Serializer {
   static readonly type_internal = '__TYPE__';
   static handlers = {
+    Object: {
+      serialize: (value: object) => {
+        const serializedObject: Record<string, any> = {};
+        for (const key in value) {
+          if (value.hasOwnProperty(key)) {
+            const propertyValue = (value as any)[key];
+            serializedObject[key] = Serializer.serialize(propertyValue);
+          }
+        }
+        return {
+          [Serializer.type_internal]: 'Object',
+          value: serializedObject,
+        };
+      },
+
+      deserialize: (value: any): object => {
+        const deserializedObject: Record<string, any> = {};
+
+        for (const key in value.value) {
+          if (value.value.hasOwnProperty(key)) {
+            const serializedPropertyValue = value.value[key];
+            deserializedObject[key] = Serializer.deserialize(serializedPropertyValue);
+          }
+        }
+
+        return deserializedObject;
+      },
+    },
     Date: {
-      serialize: (value: Date) => ({ [Serializer.type_internal]: 'Date', value: value.toISOString() }),
-      deserialize: (value: any) => new Date(value),
+      serialize: (value: Date) => {
+        return { [Serializer.type_internal]: 'Date', value: value };
+      },
+      deserialize: (value: any) => new Date(value.value),
     },
     RegExp: {
-      serialize: (value: RegExp) => ({ [Serializer.type_internal]: 'RegExp', value: value.toString() }),
-      deserialize: (value: any) => new RegExp(value),
+      serialize: (value: RegExp) => ({
+        [Serializer.type_internal]: 'RegExp',
+        value: {
+          source: value.source,
+          flags: value.flags,
+        },
+      }),
+      deserialize: (value: any) => new RegExp(value.value.source, value.value.flags),
     },
     Function: {
-      serialize: (value: Function) => ({ [Serializer.type_internal]: 'Function', value: value.toString() }),
-      deserialize: (value: any) => new Function(`return ${value}`)(),
+      serialize: (value: Function) => ({ [Serializer.type_internal]: 'Function', value: value.toString(), name: value.name }),
+      deserialize: (value: any) => {
+        const func = new Function(`return ${value.value}`)();
+        Object.defineProperty(func, 'name', { value: value.name });
+        return func;
+      },
     },
     Map: {
-      serialize: (value: Map<any, any>) => ({ [Serializer.type_internal]: 'Map', value: Array.from(value.entries()) }),
-      deserialize: (value: any) => new Map(value.value),
+      serialize: (value: Map<any, any>) => ({
+        [Serializer.type_internal]: 'Map',
+        value: Array.from(value.entries()).map(([key, val]) => {
+          return [Serializer.serialize(key), Serializer.serialize(val)];
+        }),
+      }),
+      deserialize: (value: any) =>
+        new Map(
+          value.value.map(([key, val]: [key: string, val: string]) => {
+            return [Serializer.deserialize(key), Serializer.deserialize(val)];
+          }),
+        ),
     },
     Set: {
       serialize: (value: Set<any>) => ({ [Serializer.type_internal]: 'Set', value: Array.from(value) }),
       deserialize: (value: any) => new Set(value.value),
     },
     Buffer: {
-      serialize: (value: Buffer) => ({ [Serializer.type_internal]: 'Buffer', value: value.toString('base64') }),
+      serialize: (value: Buffer) => {
+        return { [Serializer.type_internal]: 'Buffer', value: value.toString('base64') };
+      },
       deserialize: (value: any) => Buffer.from(value.value, 'base64'),
     },
     Error: {
@@ -59,15 +112,32 @@ export class Serializer {
     },
     ArrayBuffer: {
       serialize: (value: ArrayBuffer) => ({ [Serializer.type_internal]: 'ArrayBuffer', value: Array.from(new Uint8Array(value)) }),
-      deserialize: (value: any) => new ArrayBuffer(value.value),
+      deserialize: (value: any) => {
+        const byteArray = new Uint8Array(value.value);
+        return byteArray.buffer;
+      },
     },
   };
   static serialize<V>(data: V): string {
     try {
+      if (data instanceof Date) {
+        return JSON.stringify(Serializer.handlers.Date.serialize(data));
+      }
+      if (data instanceof ArrayBuffer) {
+        return JSON.stringify(Serializer.handlers.ArrayBuffer.serialize(data));
+      }
+      if (data instanceof Buffer) {
+        return JSON.stringify(Serializer.handlers.Buffer.serialize(data));
+      }
+
       return JSON.stringify(data, (key, value) => {
-        if (value && typeof value === 'object' && value.constructor) {
+        if (value && (typeof value === 'object' || typeof value === 'function') && value.constructor) {
           const typeName = value.constructor.name as keyof Handlers;
           if (Serializer.handlers[typeName]) {
+            if (value.hasOwnProperty('__SERIALIZED__')) {
+              return value;
+            }
+            value['__SERIALIZED__'] = true;
             return Serializer.handlers[typeName].serialize(value);
           }
         }
