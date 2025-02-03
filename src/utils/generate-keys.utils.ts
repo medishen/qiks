5230f336e06d0dfc7cd94a9a6c82d825ec0801c4
mahdi <bitsgenix@gmail.com>
2025-02-03 23:21:48 +0330

    feat(utils): added utility functions to generate and extract structured event keys for easier management of event types and related keys added methods to ensure proper formatting and key extraction based on defined prefixes

---
+++ src/utils/generate-keys.utils.ts
@@ -0,0 +1,18 @@
+import { EVENT_TYPE_PREFIX, INTERNAL_EVENT_PREFIX, INTERNAL_PREFIX, KEY_PREFIX } from '../common/constants';
+import { EventType } from '../common/enums';
+
+/**
+ * Generates a event key with structured components for easy extraction.
+ * @param event - The name/type of the event.
+ * @param key - Optional: A specific key related to the event.
+ * @returns A structured, unique event key.
+ */
+export const generateEventKey = (event: EventType, key?: string): string => {
+  // Base event key with event type
+  const eventKey = `${INTERNAL_PREFIX}${INTERNAL_EVENT_PREFIX}${EVENT_TYPE_PREFIX}${event}`;
+  // Optional parts of the key
+  const keyPart = key ? `:${KEY_PREFIX}${key}` : '';
+
+  // Combine all parts into a single event key
+  return `${eventKey}${keyPart}`;
+};
