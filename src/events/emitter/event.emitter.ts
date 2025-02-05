97e5aa705a47f44d9790ef85f8acc0878dfbfd43
mahdi <bitsgenix@gmail.com>
2025-02-05 17:44:09 +0330

    feat(events): The async sync mode and other items were removed and the edge cases bugs were fixed

--- src/events/emitter/event.emitter.ts
+++ src/events/emitter/event.emitter.ts
@@ -1,26 +1,32 @@
-import { EventParams, EventType } from '../../common';
+import { CacheErrorCodes, EventParams, EventType } from '../../common';
 import { CacheExceptionFactory } from '../../errors';
 import { EventRegistry } from '../systems/event-registry';
 
-export class EventEmitter<K extends EventType, V> {
-  constructor(private _registry: EventRegistry<K, V>) {}
+export class EventEmitter<E extends EventType, K, V> {
+  constructor(private _registry: EventRegistry<E, K, V>) {}
   /**
    * Emits an event, triggering the registered listeners with the provided parameters.
    * @param event - The event type to emit.
    * @param params - The parameters to pass to the event listeners.
    */
-  emit(event: K, params: EventParams<K, V>): void;
-  async emit(event: K, params: EventParams<K, V>): Promise<void>;
-  emit(event: K, params: EventParams<K, V>): any {
+  emit(event: E, params: EventParams<E, K, V>): void;
+  async emit(event: E, params: EventParams<E, K, V>): Promise<void>;
+  emit(event: E, params: EventParams<E, K, V>): any {
     const listeners = this._registry.getListeners(event);
-
+    const listenerPromises: Promise<void>[] = [];
     // Invoke all listeners
     listeners.forEach((listener) => {
       try {
-        listener(params.key, params.value);
-      } catch (error) {
-        throw CacheExceptionFactory.unexpectedError(`Error executing listener for event: ${event}`);
+        const result: any = listener(params); // Explicitly typing result
+        if (result instanceof Promise) {
+          listenerPromises.push(result); // If it's a promise, push it to the array
+        }
+      } catch (error: any) {
+        throw CacheExceptionFactory.createException(CacheErrorCodes.UNEXPECTED_ERROR, `Error executing listener for event: ${event}`, { event, error: error.message });
       }
     });
+    if (listenerPromises.length > 0) {
+      return Promise.all(listenerPromises).then(() => {});
+    }
   }
 }
--- src/events/systems/event-registry.ts
+++ src/events/systems/event-registry.ts
@@ -1,11 +1,11 @@
-import { EventCallback, EventType, generateEventKey } from '../../common';
-import { extractKey } from '../../common/utils/extract-keys.utils';
+import { EventCallback, EventType } from '../../common';
+import { extractEventKey, generateEventKey } from '../../utils';
 /**
  * EventRegistry is responsible for managing event listeners for specific event types.
  * It allows adding, removing, and retrieving listeners for various cache events
  */
-export class EventRegistry<K extends EventType, V> {
-  private _listeners: Map<string, EventCallback<K, V>[]>;
+export class EventRegistry<E extends EventType, K, V> {
+  private _listeners: Map<string, EventCallback<any, K, V>[]>;
 
   constructor() {
     this._listeners = new Map();
@@ -16,7 +16,7 @@ export class EventRegistry<K extends EventType, V> {
    * @param event - The event type to listen to.
    * @param listener - The callback function to be invoked when the event occurs.
    */
-  addListener<T extends K>(event: T, listener: EventCallback<K, V>): void {
+  addListener<T extends E>(event: T, listener: EventCallback<T, K, V>): void {
     const existingListeners = this._listeners.get(event) ?? [];
     const eventKey = generateEventKey(event);
     this._listeners.set(eventKey, [listener, ...existingListeners]);
@@ -27,7 +27,7 @@ export class EventRegistry<K extends EventType, V> {
    * @param event - The event type from which to remove the listener.
    * @param listener - The callback function to be removed.
    */
-  removeListener<T extends K>(event: T, listener: EventCallback<K, V>): void {
+  removeListener<T extends E>(event: T, listener: EventCallback<T, K, V>): void {
     const eventKey = generateEventKey(event);
     const listeners = this._listeners.get(eventKey);
     if (listeners) {
@@ -41,7 +41,7 @@ export class EventRegistry<K extends EventType, V> {
    * @param event - The event type for which to retrieve listeners.
    * @returns An array of listeners for the event, or an empty array if no listeners are found.
    */
-  getListeners<T extends K>(event: T): EventCallback<K, V>[] {
+  getListeners<T extends E>(event: T): EventCallback<T, K, V>[] {
     const eventKey = generateEventKey(event);
     return this._listeners.get(eventKey) ?? [];
   }
@@ -54,7 +54,7 @@ export class EventRegistry<K extends EventType, V> {
     const allKeys: string[] = [];
     const keys = Array.from(this._listeners.keys());
     keys.forEach((key) => {
-      allKeys.push(extractKey(key).event);
+      allKeys.push(extractEventKey(key).event);
     });
     return allKeys;
   }
